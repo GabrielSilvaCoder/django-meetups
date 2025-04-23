c6bf0fb18e0eb4dc6c9299b9163698df64c25239
Gabriel <g.silva01@icloud.com>
2025-04-23 19:40:48 -0300

    Templates setup

--- meetups/views.py
+++ meetups/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Hello Django')
+    meetups = [
+        {
+            'title': 'A First Meetup', 
+            'location': 'New York', 
+            'slug': 'a-first-meetup'
+        },
+        {
+            'title': 'A Second Meetup', 
+            'location': 'Paris', 
+            'slug': 'a-second-meetup'
+        }
+    ]
+    return render(request, 'meetups/index.html', {
+        'show_meetups' : True,
+        'meetups' : meetups
+    })
